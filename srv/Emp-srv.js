4f53a8bb0cee2445b0e64e8e37551f047be5488a
Rizwan Trainings <rizwantrainingsb82p1@gmail.com>
2026-07-17 19:44:19 +0000

    empSr.send method as an alternative to call the onpremise s4 API

--- srv/Emp-srv.js
+++ srv/Emp-srv.js
@@ -27,11 +27,17 @@ module.exports = class EmployeeService extends cds.ApplicationService {
         //     return empSrv.run(req.query);
         // });
 
-       // code to send the data to onpremise S4 after the data is created in the cloud HANA DB
+        // code to send the data to onpremise S4 after the data is created in the cloud HANA DB
         this.after('CREATE', 'Employees', async (data, req) => {
             console.log("Data after creation:", data);
             try {
-                const result = await empSrv.run(INSERT.into('EmployeeSet').entries(data));
+                //const result = await empSrv.run(INSERT.into('EmployeeSet').entries(data));
+                // we can also use the below code to send the data to on-premise S4 using the send method using POST
+               const result = await empSrv.send({
+                    method: 'POST',
+                    path: '/EmployeeSet',
+                    data: data
+                });
                 console.log("Data sent to on-premise S4 successfully:", result);
             } catch (error) {
                 console.error("Error sending data to on-premise S4:", error);
@@ -41,7 +47,13 @@ module.exports = class EmployeeService extends cds.ApplicationService {
         this.after('UPDATE', 'Employees', async (data, req) => {
             console.log("Data after update:", data);
             try {
-                const result = await empSrv.run(UPDATE('EmployeeSet').set(data).where({ Empid: data.Empid }));
+                //const result = await empSrv.run(UPDATE('EmployeeSet').set(data).where({ Empid: data.Empid }));
+                // we can also use the below code to send the data to on-premise S4 using the send method using PUT
+                const result = await empSrv.send({
+                    method: 'PUT',
+                    path: `/EmployeeSet(Empid='${data.Empid}')`,
+                    data: data
+                });
                 console.log("Data sent to on-premise S4 successfully:", result);
             } catch (error) {
                 console.error("Error sending data to on-premise S4:", error);
@@ -51,7 +63,12 @@ module.exports = class EmployeeService extends cds.ApplicationService {
         this.after('DELETE', 'Employees', async (data, req) => {
             console.log("Data after deletion:", req.data.Empid);
             try {
-                const result = await empSrv.run(DELETE.from('EmployeeSet').where({ Empid: req.data.Empid }));
+                //const result = await empSrv.run(DELETE.from('EmployeeSet').where({ Empid: req.data.Empid }));
+                // we can also use the below code to send the data to on-premise S4 using the send method using DELETE
+                const result = await empSrv.send({
+                    method: 'DELETE',
+                    path: `/EmployeeSet(Empid='${req.data.Empid}')`
+                });
                 console.log("Data sent to on-premise S4 successfully:", result);
             } catch (error) {
                 console.error("Error sending data to on-premise S4:", error);
